1eb578ca8905870f98314bebdcae986157841695
Irina Tumanova <tymanira@mail.ru>
2024-08-05 10:10:37 +0500

    картинки в примерах

--- frontend/src/shared/ui/SolutionExample/SolutionCarousel/Item/Item.tsx
+++ frontend/src/shared/ui/SolutionExample/SolutionCarousel/Item/Item.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useState } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 
 import { ExampleResponse } from '@entities/example/common';
 
@@ -19,19 +19,20 @@ export const Item: FC<Props> = typedMemo(function Item({
     'data-testid': dataTestId = 'Item',
 }) {
     const [file, setFile] = useState<File | null>(null);
+    const fileUrl = useMemo(() => file ? URL.createObjectURL(file) : file, [file]);
 
     useEffect(() => {
         if (sol.fileIdValue) {
-            async () => {
+            (async () => {
                 const a = await getFile(sol.fileIdValue ?? '');
                 setFile(a);
-            };
+            })();
         }
-    }, []);
-
+    }, [sol]);
+    console.log(file, sol.fileIdValue);
     return (
         <div className={getBemClasses(styles, 'solutionScroll')}>
-            <img />
+            {fileUrl ? <img src={fileUrl} className={getBemClasses(styles, 'img')} /> : null}
             <Text className={getBemClasses(styles, 'link')}>
                 {sol.exampleLink ?? ''}
             </Text>
