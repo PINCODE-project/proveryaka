76f32103f9fd584921d00a1b67454fdba010ddde
CatDevelop <maks.rozhkov.2004@gmail.com>
2024-08-06 14:36:25 +0500

    fix: return link

--- frontend/src/app/providers/ErrorBoundary/ErrorBoundary.tsx
+++ frontend/src/app/providers/ErrorBoundary/ErrorBoundary.tsx
@@ -33,21 +33,14 @@ export class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundarySt
                     </Typography.Title >
                     <Typography.Paragraph style={{ textAlign: 'center' }}>
                         Обновите страницу или оставьте сообщение, чтобы мы могли разобраться:{' '}
-                        {/* <a */}
-                        {/*    href={'https://docs.google.com/forms/d/e/1FAIpQLSc0bhr6uMUgHcM3COjc4LpBJ8fDG8qzvpx_rw188ot4UR7jKA/viewform?embedded=true'} */}
-                        {/*    target="_blank" */}
-                        {/*    rel="noreferrer" */}
-                        {/*    style={{ fontWeight: 600 }} */}
-                        {/* > */}
-                        {/*    форма обратной связи */}
-                        {/* </a > */}
-                        <iframe
-                            src="https://docs.google.com/forms/d/e/1FAIpQLSc0bhr6uMUgHcM3COjc4LpBJ8fDG8qzvpx_rw188ot4UR7jKA/viewform?embedded=true"
-                            width="640" height="439" frameBorder="0"
-                            marginHeight="0" marginWidth="0"
+                        <a
+                            href={'https://docs.google.com/forms/d/e/1FAIpQLSc0bhr6uMUgHcM3COjc4LpBJ8fDG8qzvpx_rw188ot4UR7jKA/viewform?embedded=true'}
+                            target="_blank"
+                            rel="noreferrer"
+                            style={{ fontWeight: 600 }}
                         >
-                            Загрузка…
-                        </iframe >
+                            форма обратной связи
+                        </a >
                     </Typography.Paragraph >
                 </Suspense >
             );
