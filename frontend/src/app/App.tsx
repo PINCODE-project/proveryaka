3291402b5c50113beaaaec679a6f24c9a39009b5
Irina Tumanova <tymanira@mail.ru>
2024-09-06 21:31:56 +0500

    feat: стили для контейнера контента

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,16 +1,10 @@
 import './styles/index.css';
-import { useTranslation } from 'react-i18next';
 
 import { AppRouter } from './providers/router/AppRouter';
 import '@shared/styles/index.css';
 
 const App = () => {
-    const { t } = useTranslation();
-    return (
-        <div>
-            <AppRouter />
-        </div>
-    );
+    return <AppRouter />;
 };
 
 export default App;
---
+++ frontend/src/shared/lib/getModuleClasses.ts
@@ -0,0 +1,38 @@
+import block from 'bem-css-modules';
+
+import { mixClasses } from './mixClasses';
+import { BemModificator, CssModule } from '../types';
+
+export function getModuleClasses(
+    cssModule: CssModule,
+    elementName: string,
+    modificator?: BemModificator | null,
+    ...extraClasses: Array<string | undefined | null>
+): string {
+    if (Object.keys(cssModule).length === 0) {
+        return mixClasses(
+            ...extraClasses,
+        );
+    }
+
+    const classes = [];
+
+    classes.push(cssModule[elementName]);
+
+    if (modificator) {
+        Object.entries(modificator).forEach(([key, value]) => {
+            if (typeof value === 'boolean') {
+                if (value) {
+                    classes.push(cssModule[key]);
+                }
+                return;
+            }
+            classes.push(cssModule[`${key}_${value}`]);
+        });
+    }
+
+    return mixClasses(
+        ...classes,
+        ...extraClasses,
+    );
+}
