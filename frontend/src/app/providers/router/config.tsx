01b76064e72bff92c02849fbe5bd17d6d361801d
Irina Tumanova <tymanira@mail.ru>
2024-09-10 13:28:34 +0500

    feat: add redirect

--- frontend/src/app/providers/router/config.tsx
+++ frontend/src/app/providers/router/config.tsx
@@ -1,7 +1,13 @@
-import { authRouteConfig } from '@pages/auth';
+import { Navigate } from 'react-router-dom';
+
+import { authRouteConfig, AuthRouter } from '@pages/auth';
 
 import { ConfigRouteProps } from '@shared/types';
 
 export const routeConfig: ConfigRouteProps[] = [
     ...authRouteConfig,
+    {
+        path: '*',
+        element: <Navigate to={AuthRouter.SignIn} />,
+    },
 ];
