b70357b28ff6a4e57026c09ffb184e8d95eadaf3
Irina Tumanova <tymanira@mail.ru>
2024-09-24 13:14:47 +0500

    fix: get files

--- frontend/src/shared/api/file/solution/getFile.ts
+++ frontend/src/shared/api/file/solution/getFile.ts
@@ -1,9 +1,12 @@
 import { solutionHttp } from '@shared/config/axios';
 
-import { extractData, replaceIfEmpty } from '../../../lib';
+import { replaceIfEmpty } from '../../../lib';
 
 export function getFile(fileId: string): Promise<File | null> {
     return solutionHttp.get<File>(`files/${fileId}`, { responseType: 'blob' })
-        .then(extractData)
+        .then(response => {
+            const match = response.headers['content-disposition'].match(/filename=([^;]+)/)?.[1] ?? '';
+            return new File([response.data], match);
+        })
         .then(replaceIfEmpty<File | null>(null));
 }
