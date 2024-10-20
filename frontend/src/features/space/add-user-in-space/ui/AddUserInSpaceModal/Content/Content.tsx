5ccaa00b417084146440e363dbae7d6b797728ae
Irina Tumanova <tymanira@mail.ru>
2024-10-20 18:53:15 +0500

    feat: 309 - suspense для добавления участников в пространство и измененияпространства

---
+++ frontend/src/features/space/add-user-in-space/ui/AddUserInSpaceModal/Content/Content.tsx
@@ -0,0 +1,104 @@
+import { Button, Flex, Form, notification, Select } from 'antd';
+import { FC, useCallback } from 'react';
+import { useQueryClient } from 'react-query';
+
+import { useAddUserInSpace } from '@features/space/add-user-in-space/lib/useAddUserInSpace';
+import { AddUserToSpaceRequest } from '@features/space/add-user-in-space/model/AddUserToSpaceRequest';
+
+import { getSpaceExpertsQueryKey, getSpaceOrganizerQueryKey, getSpaceStudentsQueryKey } from '@entities/space';
+import { SpaceRoleType } from '@entities/space/model/SpaceRoleType';
+import { useGetUserAll } from '@entities/user';
+
+import { sortSelectOptionsByLabel, typedMemo } from '@shared/lib';
+import { ClassNameProps, TestProps } from '@shared/types';
+
+import styles from './Content.module.css';
+
+export type Props = ClassNameProps & TestProps & Readonly<{
+    spaceId: string;
+    onClose: () => void;
+}>;
+
+export const Content: FC<Props> = typedMemo(function Content({
+    spaceId,
+    onClose,
+}) {
+    const [notify, contextHolder] = notification.useNotification();
+    const queryClient = useQueryClient();
+
+    const { data: users } = useGetUserAll();
+    const { mutate: add } = useAddUserInSpace({
+        onSuccess: () => {
+            queryClient.resetQueries(getSpaceStudentsQueryKey(spaceId));
+            queryClient.resetQueries(getSpaceExpertsQueryKey(spaceId));
+            queryClient.resetQueries(getSpaceOrganizerQueryKey(spaceId));
+            notify.success({
+                message: 'Пользователи добавлены в пространство',
+            });
+            onClose();
+        },
+    });
+
+    const onSubmit = useCallback(async (form: AddUserToSpaceRequest) => {
+        add({
+            ...form,
+            spaceId,
+        });
+    }, [spaceId, add]);
+
+    return (
+        <Form
+            className={styles.form}
+            name="SpaceForm"
+            layout="vertical"
+            onFinish={onSubmit}
+            requiredMark={false}
+        >
+            <Flex gap={12} vertical>
+                <Flex gap={20} vertical>
+                    <Form.Item<AddUserToSpaceRequest>
+                        className={styles.formItem}
+                        label="Пользователи"
+                        name="userProfileIdList"
+                        rules={[
+                            { required: true, message: 'Выберите пользователей' },
+                        ]}
+                    >
+                        <Select
+                            mode="multiple"
+                            placeholder="Не выбрано"
+                            filterOption={sortSelectOptionsByLabel}
+                        >
+                            {users?.userInfoList?.map(user => (
+                                <Select.Option value={user.id}>
+                                    {user.surname} {user.name} {user.patronymic}
+                                </Select.Option>
+                            ))}
+                        </Select>
+                    </Form.Item>
+
+                    <Form.Item<AddUserToSpaceRequest>
+                        className={styles.formItem}
+                        name="spaceRoleType"
+                        label="Роль"
+                        rules={[
+                            { required: true, message: 'Выберите роль' },
+                        ]}
+                    >
+                        <Select placeholder="Не выбрано" >
+                            <Select.Option value={SpaceRoleType.Student}>Студент</Select.Option>
+                            <Select.Option value={SpaceRoleType.Expert}>Эксперт</Select.Option>
+                            <Select.Option value={SpaceRoleType.Organizer}>Организатор</Select.Option>
+                        </Select>
+                    </Form.Item>
+                </Flex>
+
+                <Form.Item className={styles.submitButton}>
+                    <Button type="primary" htmlType="submit" block>
+                        Добавить
+                    </Button>
+                </Form.Item>
+            </Flex>
+        </Form>
+    );
+});
--- frontend/src/features/space/edit-space/ui/EditSpaceModal/EditSpaceModal.tsx
+++ frontend/src/features/space/edit-space/ui/EditSpaceModal/EditSpaceModal.tsx
@@ -1,20 +1,10 @@
-import { Button, Form, Modal, notification } from 'antd';
-import { FC, ReactNode, useCallback, useEffect, useState } from 'react';
-import { useQueryClient } from 'react-query';
+import { Flex, Modal, Spin } from 'antd';
+import { FC, ReactNode, Suspense, useCallback, useState } from 'react';
 
-import { getSpacesQueryKey, Space, SpaceForm } from '@entities/space';
-import { getSpaceQueryKey } from '@entities/space/lib/getSpaceQueryKey';
-import { useGetSpace } from '@entities/space/lib/useGetSpace';
-import { SpaceAccessType } from '@entities/space/model/SpaceAccessType';
-
-import { getFile } from '@shared/api';
-import { createFile } from '@shared/api/file/createFile';
 import { typedMemo } from '@shared/lib';
 import { ClassNameProps, TestProps } from '@shared/types';
 
-import styles from './EditSpaceModal.module.css';
-import { useEditSpace } from '../../lib/useEditSpace';
-import { EditSpaceRequest } from '../../model/EditSpaceRequest';
+import { Content } from './Content';
 
 export type Props = ClassNameProps & TestProps & Readonly<{
     triggerComponent: (onOpen: () => void) => ReactNode;
@@ -25,70 +15,13 @@ export const EditSpaceModal: FC<Props> = typedMemo(function EditSpaceModal({
     triggerComponent,
     spaceId,
 }) {
-    const [api, contextHolder] = notification.useNotification();
-    const queryClient = useQueryClient();
-
-    const { data: space } = useGetSpace(spaceId);
-    const [initialValues, setInitialValues] =
-        useState<EditSpaceRequest | undefined>(undefined);
-    const [initialFile, setInitialFile] = useState<File | null>(null);
-
-    const getInitialValues = useCallback(async () => {
-        if (!space) {
-            return;
-        }
-        const file = space.iconFileId ? await getFile(space.iconFileId) : null;
-        setInitialFile(file);
-
-        setInitialValues({
-            iconFileId: null,
-            name: space.name,
-            accessType: space.accessType,
-            description: space.description,
-        });
-    }, [space]);
-
-    useEffect(() => {
-        getInitialValues();
-    }, [getInitialValues]);
-
     const [isOpen, setIsOpen] = useState(false);
-    const { mutate: edit } = useEditSpace({
-        onSuccess: () => {
-            queryClient.resetQueries(getSpacesQueryKey);
-            queryClient.resetQueries(getSpaceQueryKey(spaceId));
-            api.success({
-                message: 'Пространство изменено',
-            });
-            setIsOpen(false);
-        },
-    });
-
-    const onSubmit = useCallback(async (form: EditSpaceRequest, file: File | null) => {
-        let iconFileId = form.iconFileId;
-        try {
-            if (file) {
-                iconFileId = (await createFile(file)).id;
-            }
-            // Обработка ошибки прописана в interceptor, здесь же необходим try для того
-            // чтобы избежать выпадения ошибки здесь
-        } catch {}
-
-        edit({
-            ...form,
-            id: spaceId,
-            accessType: space?.accessType ?? SpaceAccessType.Closed,
-            iconFileId: iconFileId ?? null,
-        });
-    }, [edit, spaceId, space]);
 
     const onOpen = useCallback(() => setIsOpen(true), []);
-
     const onClose = useCallback(() => setIsOpen(false), []);
 
     return (
         <>
-            {contextHolder}
             {triggerComponent(onOpen)}
             <Modal
                 title="Изменение пространства"
@@ -97,18 +30,9 @@ export const EditSpaceModal: FC<Props> = typedMemo(function EditSpaceModal({
                 onClose={onClose}
                 onCancel={onClose}
             >
-                <SpaceForm
-                    submit={onSubmit}
-                    initialValues={initialValues as Space}
-                    initialFile={initialFile}
-                    submitButton={(
-                        <Form.Item className={styles.submitButton}>
-                            <Button type="primary" htmlType="submit" block>
-                                Сохранить
-                            </Button>
-                        </Form.Item>
-                    )}
-                />
+                <Suspense fallback={<Flex align="center" justify="center"><Spin /></Flex>}>
+                    <Content spaceId={spaceId} onClose={onClose} />
+                </Suspense>
             </Modal>
         </>
     );
