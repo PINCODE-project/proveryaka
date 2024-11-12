68ab1380bdf7157bc5ca0a3151e9e4eb3fea6151
irkatyman <tymanira@mail.ru>
2024-11-12 21:55:45 +0500

    fix

--- frontend/src/pages/space/SpaceIssuePage/subpages/SpaceIssueMarks/SpaceIssueMarks.tsx
+++ frontend/src/pages/space/SpaceIssuePage/subpages/SpaceIssueMarks/SpaceIssueMarks.tsx
@@ -1,3 +1,4 @@
+import { Flex, Typography } from 'antd';
 import { FC, useCallback } from 'react';
 import { Navigate } from 'react-router-dom';
 
@@ -12,6 +13,8 @@ import { useSpaceId } from '@shared/hooks/useSpaceId';
 import { typedMemo } from '@shared/lib';
 import { ClassNameProps, TestProps } from '@shared/types';
 
+import styles from './SpaceIssueMarks.module.css';
+
 export type Props = ClassNameProps & TestProps;
 
 export const SpaceIssueMarks: FC<Props> = typedMemo(function SpaceIssueMarks() {
@@ -22,14 +25,19 @@ export const SpaceIssueMarks: FC<Props> = typedMemo(function SpaceIssueMarks() {
     const { data: solution } = useGetStudentIssueSolution(issueId ?? '', {
         enabled: isStudent,
         useErrorBoundary: false,
-        onError: () => {
-
-        },
+        onError: () => {},
     });
     const actionRender = useCallback((_: string, record: GetReviews) => 'Feedback', []);
 
     if (!isStudent) {
         return <Navigate to={SpaceRouter.SpaceIssueDescription(spaceId ?? '', issueId ?? '')} />;
+    }
+    if (!solution) {
+        return (<Flex align="center" justify="center" className={styles.emptySolutionContainer}>
+            <Typography.Text className={styles.emptySolution}>
+                Работа еще не сдана
+            </Typography.Text>
+        </Flex>);
     }
     return (
         <SolutionMarksTable
